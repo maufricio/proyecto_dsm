9c173cb9ddcc7f02698ad49694413d13a48ce3b3
jacquelinechavez30 <notaschavez@gmail.com>
2025-03-16 18:45:15 -0600

    Pantalla de comidas parte 1

--- app/src/main/java/com/ps212544_ml211022_cr200574_al202809/myapplication/MainActivity.kt
+++ app/src/main/java/com/ps212544_ml211022_cr200574_al202809/myapplication/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.ps212544_ml211022_cr200574_al202809.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +20,22 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        // boton para login
+        val botonlogin = findViewById<ImageView>(R.id.botonlogin)
+        botonlogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity ::class.java)
+            startActivity(intent)
+        }
+        val botontiempodecomida = findViewById<Button>(R.id.botontiempodecomida)
+        val tiempodecomida = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+
+        //Dependiendo la hora del dia se mostrara un titulo diferente
+        botontiempodecomida.text = when (tiempodecomida) {
+            in 0..11 -> "Desayunos > "
+            in 12..17 -> "Almuerzos > "
+            else -> "Cenas > "
+        }
+
+
     }
 }
