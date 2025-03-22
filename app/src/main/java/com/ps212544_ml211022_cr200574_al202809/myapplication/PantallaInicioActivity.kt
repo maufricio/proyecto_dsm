fd27fd4a5d790571ab31eaf2cec16047fe1e1c76
mauricio.perez <mauriciomaps42@gmail.com>
2025-03-22 13:23:41 -0600

    dev: Avane 2 de Dashboard terminado y contenedores de comida.

--- app/src/main/java/com/ps212544_ml211022_cr200574_al202809/myapplication/PantallaInicioActivity.kt
+++ app/src/main/java/com/ps212544_ml211022_cr200574_al202809/myapplication/PantallaInicioActivity.kt
@@ -10,11 +10,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class PantallaInicioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_pantallainicio)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
